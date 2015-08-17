86c055aa9b48933991742169c9065b609481fe8f
vichek <v.veshchinskaya@gmail.com>
2015-08-17 04:49:54 +0300

    Added message about unsupported file format. Cleanup code

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# webplayer
-РўРµСЃС‚РѕРІРѕРµ Р·Р°РґР°РЅРёРµ 3 РґР»СЏ РЁР Р
-Web Player
+Тестовое задание 3 для ШРИ - Web Audio Player.
+
+Примечание: область для drop - левый блок "Now you are listening to".
+
+Рабочая ссылка:  http://vichek.github.io/webplayer
+
--- player.js
+++ player.js
@@ -1,3 +1,7 @@
+//$$ = document.querySelectorAll.bind(document);
+//$ = document.querySelector.bind(document);
+
+"use strict";
 
 // Global Variables for Audio
 var audioContext = null;
@@ -20,52 +24,26 @@ var jazz = [3.5, 2.5, 1, 2, -2, -2, -0.5, 1, 2.5, 3.5];
 var classical = [5, 4, 3, 2, -1.5, -1.5, 0, 2, 3, 4];
 var presets = [];
 
-$$ = document.querySelectorAll.bind(document);
-$ = document.querySelector.bind(document);
-
-// Hacks to deal with different function names in different browsers
-requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            function (callback, element) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-})();
-
-AudioContext = (function () {
-    return window.AudioContext;
-})();
-
 function createContext() {
-    var previous = window.equalizer;
     audio = new Audio();
     audio.crossorigin = "anonymous";
     audio.controls = true;
 
-    // avoid multiple AudioContext creating
-    if (previous && previous.audioContext) {
-        audioContext = previous.audioContext;
-    } else {
-        audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    }
+    audioContext = new AudioContext();
     sourceNode = audioContext.createMediaElementSource(audio);
-};
+}
 
 /**
  * creates 10 input elements
  */
 function createInputs(className, container) {
-    var inputs = [],
-      node,
-      i;
+    var inputs = [], node, i;
 
     document.write('<script src="binarybuffer.js"><\/script>');
     document.write('<script src="ID3v2.js"><\/script>');
 
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i = i + 1) {
         node = document.createElement('input');
-        // remove dot
         node.className = className;
         node.setAttribute('min', -12);
         node.setAttribute('max', 12);
@@ -78,7 +56,7 @@ function createInputs(className, container) {
     }
 
     return inputs;
-};
+}
 
 /**
  * bind almost all events here
@@ -93,7 +71,7 @@ function initEvents(inputs) {
 
     document.getElementById('presetList').addEventListener('change', function (e) {
         var i;
-        for (i = 0; i < 10; i++) {
+        for (i = 0; i < 10; i = i + 1) {
             if (inputs[i]) {
                 inputs[i].value = presets[e.target.selectedIndex][i];
             }
@@ -109,7 +87,11 @@ function initEvents(inputs) {
         }
 
         var audiofile = e.target.files[0];
-        getAudioFile(audiofile);
+        if (audiofile.type.match(/audio.*/)) {
+            getAudioFile(audiofile);
+        } else {
+            alert("Unsupported file format");
+        }
 
     }, false);
 
@@ -120,7 +102,6 @@ function initEvents(inputs) {
     document.getElementById('play').addEventListener('click', function (e) {
         e.preventDefault();
         playSound();
-        console.log(e);
     });
 
     document.getElementById('stop').addEventListener('click', function (e) {
@@ -135,41 +116,40 @@ function initEvents(inputs) {
 
     var dropZone =  document.getElementById('dropzone');
 
-    // Optional.   Show the copy icon when dragging over.  Seems to only work for chrome.
     dropZone.addEventListener('dragover', function(e) {
         e.stopPropagation();
         e.preventDefault();
         e.dataTransfer.dropEffect = 'copy';
     });
 
-    // Get file data on drop
     dropZone.addEventListener('drop', function(e) {
         e.stopPropagation();
         e.preventDefault();
-        var file = e.dataTransfer.files[0]; // Array of all files
-        if (file.type.match(/audio.*/)) {
-            getAudioFile(file);
+        var dropfile = e.dataTransfer.files[0]; // Array of all files
+        if (dropfile.type.match(/audio.*/)) {
+            getAudioFile(dropfile);
+        } else {
+            alert("Unsupported file format");
         }
     });
-};
+}
 
 function getAudioFile(audiofile) {
-        if (audiofile.type == 'audio/mp3' || audiofile.type == 'audio/mpeg') {
-            var tag = new ID3v2;
+    if (audiofile.type === 'audio/mp3' || audiofile.type === 'audio/mpeg') {
+        var tag = new ID3v2();
 
-            tag.readFromFile(audiofile, function (tag) {
-                var str = tag.get('TIT2') + " by " + tag.get('TPE1') + " from " + tag.get('TALB');
-                console.log(tag);
-                document.getElementById('currentSong').innerHTML = str;
-            });
+        tag.readFromFile(audiofile, function (tag) {
+            var str = tag.get('TIT2') + " by " + tag.get('TPE1') + " from " + tag.get('TALB');
+            document.getElementById('currentSong').innerHTML = str;
+        });
 
-        }
-        else {
-            document.getElementById('lblMarquee').innerHTML = "Unsupported file type " + audiofile.type;
-        }
-        urlvar = URL.createObjectURL(audiofile);
-        stopPlay();
-        document.getElementById('play').className = "button-play";
+    }
+    else {
+        document.getElementById('lblMarquee').innerHTML = "Unsupported file type " + audiofile.type;
+    }
+    urlvar = URL.createObjectURL(audiofile);
+    stopPlay();
+    document.getElementById('play').className = "button-play";
 }
 
 /**
@@ -184,7 +164,7 @@ function createFrequencyFilter(frequency) {
     filter.Q.value = 1;
 
     return filter;
-};
+}
 
 /**
  * create filter for each frequency and for audio analyzer
@@ -198,7 +178,7 @@ function createFilters() {
     analyserNode.smoothingTimeConstant = 0.3;
     analyserNode.fftSize = 1024;
     // Create the array for the data values
-    frequencyArray = new Uint8Array(analyserNode.frequencyBinCount);    // **
+    frequencyArray = new Uint8Array(analyserNode.frequencyBinCount);
 
     // setup a javascript node
     javascriptNode = audioContext.createScriptProcessor(1024, 1, 1);
@@ -207,15 +187,10 @@ function createFilters() {
     // we use information from the analyzer node
     // to draw the volume
     javascriptNode.onaudioprocess = function (e) {
-        //      console.log('onaudioprocess');
-        //      var curtime = parseInt(e.playbackTime, 10);
-        //      seek.setAttribute("value", curtime);
-
         // get the average for the first channel
         analyserNode.getByteFrequencyData(frequencyArray);
 
-        requestAnimFrame(drawFrequencyDomain);   // **
-        //    drawFrequencyDomain();
+        drawFrequencyDomain();
     };
 
     // create filterNodes
@@ -228,55 +203,38 @@ function createFilters() {
         prev.connect(curr);
         return curr;
     });
-};
+}
 
 function drawFrequencyDomain() {
+    var i, y;
     clearCanvas();
-    ctx.fillStyle = '#FFFFFF';                             // **
-    for (var i = 0; i < frequencyArray.length; i++) {
-        var y = canvasHeight - Math.round(frequencyArray[i]);  // **
-        ctx.fillRect(i, 0, 1, y);                                 // **
+    ctx.fillStyle = '#FFFFFF';
+    for (i = 0; i < frequencyArray.length; i = i + 1) {
+        y = canvasHeight - Math.round(frequencyArray[i]);
+        ctx.fillRect(i, 0, 1, y);
     }
-};
+}
 
 function clearCanvas() {
-    ctx.fillStyle = gradient;   // **
+    ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-};
+}
 
 // load the specified sound
 function playSound() {
-    if (urlvar != null && !played) {
+    if (urlvar !== null && !played) {
         audio.src = urlvar;
-        //        var request = new XMLHttpRequest();
-        //        request.open('GET', urlvar, true);
-        //        request.responseType = 'arraybuffer';
-
-        // When loaded decode the data
-        //        request.onload = function() {
-
-        // decode the data
-        //            audioContext.decodeAudioData(request.response, function(buffer) {
-        // when the audio is decoded play the sound
-        //              sourceNode = audioContext.createBufferSource(); // Create sound source
         sourceNode.connect(filterNodes[0]);
         filterNodes[9].connect(analyserNode);
         analyserNode.connect(javascriptNode);
         javascriptNode.connect(audioContext.destination);
         filterNodes[9].connect(audioContext.destination);
-        //              sourceNode.buffer = buffer;
-        //              sourceNode.start(0);
         audio.play();
-        //            seek.setAttribute('max', audio.duration);
-        console.log('duration = ' + audio.duration);
         document.getElementById('play').className = "button-play-hide";
         document.getElementById('stop').className = "button-stop";
         played = true;
-        //            }, onError);
-        //        }
-        //        request.send();
     }
-};
+}
 
 function stopPlay() {
     if (played) {
@@ -289,12 +247,12 @@ function stopPlay() {
         document.getElementById('stop').className = "button-stop-hide";
         played = false;
     }
-};
+}
 
 // log if an error occurs
 function onError(e) {
     console.log(e);
-};
+}
 
 
 /**
@@ -318,7 +276,7 @@ presets[2] = rock;
 presets[3] = jazz;
 presets[4] = classical;
 
-var container = $(".eq-wrap");
+var container = document.querySelector(".eq-wrap");
 var inputs = createInputs('slider', container);
 
 createContext();
